88307b92d74fe130dae0c85f3b873804c1803ffe
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-04-05 10:42:07 +0200

    added slide in animation to the navbar

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
+  //Toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 1s ease forwards ${
+          index / 5 + 0.4
+        }s`;
+      }
+    });
   });
 };
 
